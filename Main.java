c543068c6e61570b03410ef7eea3807a235e9ace
Gormi3815 <gormi3815@wrdsb.ca>
2020-10-09 04:12:34 +0000

    made it so it will tell the person their bank balance over time

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
-/**
+import java.util.Scanner;
+/** this program will tell you your balance over a period of time due to intrest
  *
- * @author 
+ * @author isaac
  */
 public class Main {
 
@@ -10,6 +11,21 @@ public class Main {
    */
   public static void main(String[] args) {
     // TODO code application logic here
+    Scanner input = new Scanner(System.in);
     
+    System.out.println("Please enter your starting balance");
+    double balance = input.nextDouble();
+    
+    System.out.println("Please enter your intrest rate");
+    double intrest = input.nextDouble()/100;
+    System.out.println("Please enter the number of years you will invest for.");
+    double years = input.nextInt();
+   // intrest x years 5%x5 =1.25
+    for (double count = 0; count < 1 ; count++){
+      double rsum = (intrest*years) +1;
+     double sum = balance* rsum;
+     //double money =intrest*sum;
+     System.out.print("Your final balance will be " + sum);
+    }
   }
 }
